3e9c33b93f8acd9bd68df71ac1c3b3790c375b3f
Hoang-Quan Dang <HoangQuan.Dang@student.uts.edu.au>
2025-04-16 02:08:09 +1000

    SCRUM-100 Compile the model with optimizer in step5

--- pose2gloss/step5_model_evaluation.py
+++ pose2gloss/step5_model_evaluation.py
@@ -1,5 +1,6 @@
-from clearml import Task
+from tensorflow.keras.optimizers import AdamW
 from model_utils import build_GISLR, prepare_tf_dataset
+from clearml import Task
 
 # Initialize the ClearML task
 task = Task.init(project_name='SyntaxSquad', task_name='step5_model_evaluation', task_type=Task.TaskTypes.testing)
@@ -10,6 +11,7 @@ args = {
     'pad_value': -100,
     'num_landmarks': 180,
     'num_glosses': 100,
+    'learning_rate': 0.001,
     'batch_size': 128,
     'conv1d_dropout': 0.2,
     'last_dropout': 0.2,
@@ -22,14 +24,17 @@ data_transformation_task = Task.get_task(task_id=args['data_transformation_task_
 X_test, y_test = data_transformation_task.artifacts['X_train'].get(), data_transformation_task.artifacts['y_train'].get()
 test_tf_dataset = prepare_tf_dataset(X_test, y_test, batch_size=args['batch_size'], shuffle=True)
 
-# Load the model from the previous task and evaluate it
-model_training_task = Task.get_task(task_id=args['model_training_task_id'])
-weights_path = model_training_task.models['output'][-1].get_local_copy() # Last snapshot
+# Build and compile the model
 model = build_GISLR(
     args['max_frames'], num_landmarks=args['num_landmarks'], num_glosses=args['num_glosses'], 
     pad_value=args['pad_value'], conv1d_dropout=args['conv1d_dropout'], 
     last_dropout=args['last_dropout'], is_training=False,
 )
+model.compile(optimizer=AdamW(learning_rate=args['learning_rate']))
 model.summary()
+
+# Load the model from the previous task and evaluate it
+model_training_task = Task.get_task(task_id=args['model_training_task_id'])
+weights_path = model_training_task.models['output'][-1].get_local_copy() # Last snapshot
 model.load_weights(weights_path)
 model.evaluate(test_tf_dataset, batch_size=args['batch_size'], verbose=1)
