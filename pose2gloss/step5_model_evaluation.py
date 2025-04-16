c8180e94c64ec910507bbd0ef1422eab8c74a4ed
Hoang-Quan Dang <HoangQuan.Dang@student.uts.edu.au>
2025-04-16 20:12:05 +1000

    SCRUM-102 Refactor model evaluation to update data retrieval and logging series for clarity

--- pose2gloss/step5_model_evaluation.py
+++ pose2gloss/step5_model_evaluation.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
-from model_utils import build_and_compile_GISLR, prepare_tf_dataset
-from sklearn.metrics import classification_report, confusion_matrix
 from clearml import Task
+from sklearn.metrics import classification_report, confusion_matrix
+from model_utils import build_and_compile_GISLR, prepare_tf_dataset
 
 # Initialize the ClearML task
 Task.add_requirements('nvidia-cudnn-cu12', '9.3.0.75')
@@ -25,7 +25,7 @@ data_transformation_task = Task.get_task(task_id=args['data_transformation_task_
 X_train, y_train = data_transformation_task.artifacts['X_train'].get(), data_transformation_task.artifacts['y_train'].get()
 X_val, y_val = data_transformation_task.artifacts['X_val'].get(), data_transformation_task.artifacts['y_val'].get()
 X_test, y_test = data_transformation_task.artifacts['X_test'].get(), data_transformation_task.artifacts['y_test'].get()
-label_encoder = data_transformation_task.artifacts['label_encoder'].get()
+gloss_labels = data_transformation_task.artifacts['label_encoder'].get().classes_
 num_landmarks, num_glosses = X_train.shape[-2], len(set(y_train))
 
 # Build and compile the model
@@ -33,17 +33,19 @@ model = build_and_compile_GISLR(
     args['max_frames'], num_landmarks=num_landmarks, num_glosses=num_glosses, pad_value=args['pad_value'], 
     conv1d_dropout=args['conv1d_dropout'], last_dropout=args['last_dropout'], learning_rate=args['learning_rate'], is_training=False,
 )
-model.summary(print_fn=lambda x: task.logger.report_text(x))
+model.summary()
 
 # Load the model from the previous task and evaluate it
 model_training_task = Task.get_task(task_id=args['model_training_task_id'])
 weights_path = model_training_task.models['output'][-1].get_local_copy() # Last snapshot
 model.load_weights(weights_path)
 
-# Evaluation
+# # Prepare the TF dataset for efficient data loading
 train_tf_dataset = prepare_tf_dataset(X_train, y_train, batch_size=args['batch_size'], shuffle=True)
 val_tf_dataset = prepare_tf_dataset(X_val, y_val, batch_size=args['batch_size'], shuffle=False)
 test_tf_dataset = prepare_tf_dataset(X_test, y_test, batch_size=args['batch_size'], shuffle=True)
+
+# Evaluation
 train_loss, train_accuracy, train_top5_accuracy = model.evaluate(train_tf_dataset, batch_size=args['batch_size'], verbose=1)
 val_loss, val_accuracy, val_top5_accuracy = model.evaluate(val_tf_dataset, batch_size=args['batch_size'], verbose=1)
 test_loss, test_accuracy, test_top5_accuracy = model.evaluate(test_tf_dataset, batch_size=args['batch_size'], verbose=1)
@@ -53,16 +55,15 @@ metrics_table = pd.DataFrame({
     'Train': [train_loss, train_accuracy, train_top5_accuracy],
     'Val': [val_loss, val_accuracy, val_top5_accuracy],
     'Test': [test_loss, test_accuracy, test_top5_accuracy]
-})
-task.logger.report_table(table_plot=metrics_table, title='Model evaluation metrics on 3 subsets', series='ignored')
+}).T
+task.logger.report_table(table_plot=metrics_table, title='Model evaluation metrics on 3 subsets', series='Statistics')
 
 y_test_preds = np.argmax(model.predict(test_tf_dataset), axis=1)
-classification_report_df = pd.DataFrame(classification_report(y_test, y_test_preds, output_dict=True, target_names=label_encoder.classes_)).T
-task.logger.report_table(table_plot=classification_report_df, title='Classification report on test set', series='ignored')
+classification_report_df = pd.DataFrame(classification_report(y_test, y_test_preds, output_dict=True, target_names=gloss_labels, zero_division=0)).T
+task.logger.report_table(table_plot=classification_report_df, title='Classification report on test set', series='Statistics')
 
 cm = confusion_matrix(y_test, y_test_preds)
 task.logger.report_confusion_matrix(
-    matrix=cm, title='Confusion matrix', series='ignored',
-    xlabels=label_encoder.classes_, ylabels=label_encoder.classes_,
-    xaxis='Predicted gloss', yaxis='True gloss',
+    matrix=cm, title='Confusion matrix on test set', series='ignored',
+    xlabels=gloss_labels, ylabels=gloss_labels, xaxis='Predicted gloss', yaxis='True gloss',
 )
