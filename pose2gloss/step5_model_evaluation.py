8ced003f90a7f057c1439bc6d7582b8727c0688f
Hoang-Quan Dang <HoangQuan.Dang@student.uts.edu.au>
2025-04-16 19:39:48 +1000

    SCRUM-101 Enhance model evaluation report

--- pose2gloss/step5_model_evaluation.py
+++ pose2gloss/step5_model_evaluation.py
@@ -1,4 +1,7 @@
+import numpy as np
+import pandas as pd
 from model_utils import build_and_compile_GISLR, prepare_tf_dataset
+from sklearn.metrics import classification_report, confusion_matrix
 from clearml import Task
 
 # Initialize the ClearML task
@@ -17,25 +20,49 @@ args = {
 task.connect(args)
 task.execute_remotely()
 
-# Get test data
+# Get the artifacts from data transformation task
 data_transformation_task = Task.get_task(task_id=args['data_transformation_task_id'])
 X_train, y_train = data_transformation_task.artifacts['X_train'].get(), data_transformation_task.artifacts['y_train'].get()
+X_val, y_val = data_transformation_task.artifacts['X_val'].get(), data_transformation_task.artifacts['y_val'].get()
 X_test, y_test = data_transformation_task.artifacts['X_test'].get(), data_transformation_task.artifacts['y_test'].get()
+label_encoder = data_transformation_task.artifacts['label_encoder'].get()
 num_landmarks, num_glosses = X_train.shape[-2], len(set(y_train))
-
-# Prepare the TF dataset for efficient data loading
-train_tf_dataset = prepare_tf_dataset(X_train, y_train, batch_size=args['batch_size'], shuffle=True)
-test_tf_dataset = prepare_tf_dataset(X_test, y_test, batch_size=args['batch_size'], shuffle=True)
 
 # Build and compile the model
 model = build_and_compile_GISLR(
     args['max_frames'], num_landmarks=num_landmarks, num_glosses=num_glosses, pad_value=args['pad_value'], 
     conv1d_dropout=args['conv1d_dropout'], last_dropout=args['last_dropout'], learning_rate=args['learning_rate'], is_training=False,
 )
-model.summary()
+model.summary(print_fn=lambda x: task.logger.report_text(x))
 
 # Load the model from the previous task and evaluate it
 model_training_task = Task.get_task(task_id=args['model_training_task_id'])
 weights_path = model_training_task.models['output'][-1].get_local_copy() # Last snapshot
 model.load_weights(weights_path)
-model.evaluate(test_tf_dataset, batch_size=args['batch_size'], verbose=1)
+
+# Evaluation
+train_tf_dataset = prepare_tf_dataset(X_train, y_train, batch_size=args['batch_size'], shuffle=True)
+val_tf_dataset = prepare_tf_dataset(X_val, y_val, batch_size=args['batch_size'], shuffle=False)
+test_tf_dataset = prepare_tf_dataset(X_test, y_test, batch_size=args['batch_size'], shuffle=True)
+train_loss, train_accuracy, train_top5_accuracy = model.evaluate(train_tf_dataset, batch_size=args['batch_size'], verbose=1)
+val_loss, val_accuracy, val_top5_accuracy = model.evaluate(val_tf_dataset, batch_size=args['batch_size'], verbose=1)
+test_loss, test_accuracy, test_top5_accuracy = model.evaluate(test_tf_dataset, batch_size=args['batch_size'], verbose=1)
+
+metrics_table = pd.DataFrame({
+    'Metric': ['Loss', 'Accuracy', 'Top-5 Accuracy'],
+    'Train': [train_loss, train_accuracy, train_top5_accuracy],
+    'Val': [val_loss, val_accuracy, val_top5_accuracy],
+    'Test': [test_loss, test_accuracy, test_top5_accuracy]
+})
+task.logger.report_table(table_plot=metrics_table, title='Model evaluation metrics on 3 subsets', series='ignored')
+
+y_test_preds = np.argmax(model.predict(test_tf_dataset), axis=1)
+classification_report_df = pd.DataFrame(classification_report(y_test, y_test_preds, output_dict=True, target_names=label_encoder.classes_)).T
+task.logger.report_table(table_plot=classification_report_df, title='Classification report on test set', series='ignored')
+
+cm = confusion_matrix(y_test, y_test_preds)
+task.logger.report_confusion_matrix(
+    matrix=cm, title='Confusion matrix', series='ignored',
+    xlabels=label_encoder.classes_, ylabels=label_encoder.classes_,
+    xaxis='Predicted gloss', yaxis='True gloss',
+)
