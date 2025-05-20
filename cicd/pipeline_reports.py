4fd9540e9afa5574cb3f2ba1c90054ca4eae2d74
Hoang-Quan Dang <HoangQuan.Dang@student.uts.edu.au>
2025-05-20 18:11:53 +1000

    SCRUM-135 Report last table of step7 for report-pipeline-metrics job

--- cicd/pipeline_reports.py
+++ cicd/pipeline_reports.py
@@ -20,34 +20,22 @@ def get_pipeline_of_current_commit(commit_id): # Find the ClearML task that corr
             return PipelineController.get(pipeline_id=task['id'])
             
 
-def create_output_tables(retrieve_scalars_dict): # Extract data from ClearML into format for tabulation
-    data = []
-    for graph_title, graph_values in retrieve_scalars_dict.items():
-        graph_data = []
-        for series, series_values in graph_values.items():
-            graph_data.append((graph_title, series, *series_values.values()))
-        data += graph_data
-    return sorted(data, key=lambda output: (output[0], output[1]))
+def get_reported_table(task, title): # Get the table from the task's reported plots
+    reported_plots = task.get_reported_plots()
+    metrics_report = next((d for d in reported_plots if title.lower() in d.get('metric').lower()), None)
+    if not metrics_report: return None
+    metrics_report = json.loads(metrics_report['plot_str'])['data'][0]
+    df = pd.DataFrame(list(zip(*metrics_report['cells']['values'])), columns=[col[0] for col in metrics_report['header']['values']])
+    return tabulate(df, tablefmt='github', headers='keys', showindex=False)
 
 
-def create_comment_output(task, status): # Create a markdown table from a ClearML task's output scalars
-    retrieve_scalars_dict = task.get_last_scalar_metrics()
-    if retrieve_scalars_dict:
-        scalars_tables = create_output_tables(retrieve_scalars_dict)
-        df = pd.DataFrame(data=scalars_tables, columns=['Title', 'Series', 'Last', 'Min', 'Max'])
-        df.style.set_caption(f'Last scalars metrics for task {task.task_id}, task status {status}')
-        table = tabulate(df, tablefmt='github', headers='keys', showindex=False)
-        return table
-
-
-def get_task_stats(task): # Get the comment markdown for a stats table based on the task object
+def get_task_stats(task, title): # Get the comment markdown for a stats table based on the task object
     task_status = task.get_status() # Try to get the task stats
     output_log_page = task.get_output_log_web_page()
 
     if task_status == 'completed':
-        table = create_comment_output(task, task_status)
-        if table: return f'Model performance on test set:\n\n{table}\n\n' \
-                         f'You can view full task results [here]({output_log_page})'
+        table = get_reported_table(task, title)
+        if table: return f'{title}:\n\n{table}\n\nYou can view full task results [here]({output_log_page})'
         return (f'Something went wrong when creating the task table. '
                 f'Check full task [here]({output_log_page})')
     return f'Task is in {task_status} status, this should not happen!'
@@ -70,10 +58,10 @@ def create_stats_comment(task_stats): # Create a comment on the current PR conta
 
 
 if __name__ == '__main__': # Main check: Does a ClearML task exist for this specific commit?
-    print(f"Running on commit hash: {os.getenv('COMMIT_ID')}")
+    print('Running on commit hash:', os.getenv('COMMIT_ID'))
     ml_pipeline_task = get_pipeline_of_current_commit(os.getenv('COMMIT_ID'))
     ml_pipeline_task.add_tags(['main_branch']) # If the task exists, tag it as such, so we know in the interface which one it is
     ml_pipeline_nodes = ml_pipeline_task.get_processed_nodes()
     model_evaluation_task = Task.get_task(task_id=ml_pipeline_nodes['step7_model_evaluation'].executed)
-    task_stats = get_task_stats(model_evaluation_task)
+    task_stats = get_task_stats(model_evaluation_task, 'Model evaluation metrics on 3 subsets')
     create_stats_comment(task_stats) # Get the metrics from the task and create a comment on the PR
